2ecf6f9630f7d727c5f805205ea047213e010829
Aman Dubey <117275812+aman1804@users.noreply.github.com>
2024-08-01 17:42:34 +0530

    add BASE_URL in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className="mt-auto text-white-50">
-    <p>Cover template for <a href="https://getbootstrap.com/" className="text-white">Bootstrap</a>, by <a href="https://twitter.com/mdo" className="text-white">@mdo</a>.</p>
+    <p>Cover template for <a href={`${process.env.BASE_URL}`} className="text-white">Bootstrap</a>, by <a href="https://twitter.com/mdo" className="text-white">@mdo</a>.</p>
   </footer>
   )
 }

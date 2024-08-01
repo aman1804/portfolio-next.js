c91abad0f90ba942d971225726865b652ed54a3f
Aman Dubey <117275812+aman1804@users.noreply.github.com>
2024-08-01 23:08:08 +0530

    add post in  /api/skills/route.js

--- src/app/api/personal-info/route.js
+++ src/app/api/personal-info/route.js
@@ -11,3 +11,4 @@ export async function GET(req) {
     return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
   }
 }
+
--- src/app/api/skills/route.js
+++ src/app/api/skills/route.js
@@ -14,3 +14,45 @@ export async function GET() {
     return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 });
   }
 }
+
+export async function POST(request,{params}) {
+  const db = await openDB();
+  const data = await request.json();
+  const { id } = params
+
+  if (!Array.isArray(data) || data.length === 0) {
+    return NextResponse.json({ error: 'Invalid input' }, { status: 400 });
+  }
+
+  try {
+    // Start a transaction
+    await db.exec('BEGIN TRANSACTION');
+
+    for (const skill of data) {
+      if (skill.id) {
+        // Update existing records
+        await db.run(
+          `UPDATE Skills SET user_id = ?, skill_name = ?, proficiency_level = ? WHERE id = ?`,
+          [id, skill.skill_name, skill.proficiency_level, skill.id]
+        );
+      } else {
+        // Insert new records
+        await db.run(
+          `INSERT INTO Skills (user_id, skill_name, proficiency_level) 
+          VALUES (?, ?, ?)`,
+          [id, skill.skill_name, skill.proficiency_level]
+        );
+      }
+    }
+
+    // Commit the transaction
+    await db.exec('COMMIT');
+    
+    return NextResponse.json({ success: true , message:"Data Save Successfully!"},{status:200});
+  } catch (error) {
+    // Rollback the transaction on error
+    await db.exec('ROLLBACK');
+    
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
